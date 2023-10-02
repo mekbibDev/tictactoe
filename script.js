f235ee2d7463a95cba6d205be89be7f4b01003cc
kazumaneet <mekbibtare@gmail.com>
2023-10-02 12:23:57 +0300

    Add style to page and format code

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 var submitButton = document.querySelector('#submit');
 var nameInput = document.querySelector('#name');
-submitButton.addEventListener('click', () => {
-  var name = nameInput.value;
-  var container = document.querySelector('.container');
-  container.inert = false;
-  gameController.getActivePlayer().setName(name);
-})
+var container = document.querySelector('.container');
+
+submitButton.addEventListener('click', getUserName)
 
 function gameBoard() {
 
@@ -21,9 +18,11 @@ function gameBoard() {
     else
       return false;
   }
+
   var setBoard = (board) => {
     _board = board;
   }
+
   var resetBoard = () => {
     _board = _createBoard();
   };
@@ -49,29 +48,28 @@ function player(namePassed, marker) {
   function setName(namePassed) {
     name = namePassed;
   }
-  return { getName, getMarker,setName };
+  return { getName, getMarker, setName };
 }
-
 
 function computer(name, marker) {
   var getName = () => name;
   var getMarker = () => marker;
   var isBot = true;
 
-
   function chooseCell() {
     if (gameController.getCells() === 9) {
-      let row = Math.floor(Math.random() * 3);
-      let column = Math.floor(Math.random() * 3);
+      const row = Math.floor(Math.random() * 3);
+      const column = Math.floor(Math.random() * 3);
       let state = { row, column, points: 0 };
 
       return state;
     }
+
     var state = _chooseState(JSON.parse(JSON.stringify(gameController.gameBoard.getBoard())), gameController.getCells());
     return state;
   }
-  function _chooseState(currentBoard, cells) {
 
+  function _chooseState(currentBoard, cells) {
     var states = [];
     var gameBoardTemp = gameBoard();
 
@@ -83,35 +81,38 @@ function computer(name, marker) {
           let calculatedPoints = _calculatePoints(row, column, gameBoardTemp, cells);
           states.push({ row, column, points: calculatedPoints });
         }
-
       }
     }
-    let state = states.toSorted((a, b) => a.points - b.points)[states.length - 1];
+
+    var state = states.toSorted((a, b) => a.points - b.points)[states.length - 1];
     console.log('in choose state');
     console.log(states);
-    return state;
 
+    return state;
   }
+
   function _calculatePoints(row, column, gameBoardTemp, cells, marker = 'O', depth = 0) {
     var gameBoardTempTwo = gameBoard();
     var boardCopy = JSON.parse(JSON.stringify(gameBoardTemp.getBoard()))
     gameBoardTempTwo.setBoard(boardCopy);
     gameBoardTempTwo.placeMarker(row, column, marker);
     boardCopy = JSON.parse(JSON.stringify(gameBoardTempTwo.getBoard()))
-
     cells -= 1;
+
     if (typeof (gameController.checkWin(gameBoardTempTwo.getBoard())) === 'string') {
       if (gameController.checkWin(gameBoardTempTwo.getBoard()) === 'X')
         return depth - 10;
       else
         return 10 - depth;
     }
+
     else if (cells === 0) {
       return 0;
     }
+
     depth += 1;
     marker = marker === 'X' ? 'O' : 'X';
-    let states = [];
+    var states = [];
     for (const row in gameBoardTempTwo.getBoard()) {
       for (const column in gameBoardTempTwo.getBoard()[row]) {
         if (gameBoardTempTwo.getBoard()[row][column] === null) {
@@ -120,21 +121,20 @@ function computer(name, marker) {
         }
       }
     }
+    var state = [];
 
-    let state = [];
     if (marker === 'X')
       state = states.toSorted((a, b) => a.points - b.points)[0];
     else
       state = states.toSorted((a, b) => a.points - b.points)[states.length - 1];
     console.log(states);
+
     return state.points;
-
   }
-
-
 
   return { getName, getMarker, chooseCell, isBot };
 }
+
 var gameController = (function (gameBoard) {
   var _players = [player('player', 'X'), computer('Unbeatable Bot', 'O')];
   var _cells = 9;
@@ -164,6 +164,7 @@ var gameController = (function (gameBoard) {
     }
     return _gameStatus;
   }
+
   function checkWin(board) {
     //checks for horizontal or vertical matches
     for (const i in board) {
@@ -178,9 +179,11 @@ var gameController = (function (gameBoard) {
       return board[1][1];
     return false;
   }
+
   function _switchPlayer() {
     _activePlayer = _activePlayer === _players[0] ? _players[1] : _players[0];
   }
+
   function resetGame() {
     _cells = 9;
     _gameStatus = {
@@ -190,12 +193,15 @@ var gameController = (function (gameBoard) {
     }
     gameBoard.resetBoard();
   }
+
   function getCells() {
     return _cells;
   }
+
   function getActivePlayer() {
     return _activePlayer;
   }
+
   return { playRound, resetGame, checkWin, getCells, getActivePlayer, gameBoard };
 })(gameBoard())
 
@@ -214,25 +220,26 @@ function displayBoard(board) {
   }
 
   resetButton.textContent = 'Reset';
+  resetButton.classList = 'reset'
   resetButton.addEventListener('click', resetGame);
-  boardContainer.append(resetButton);
+  container.append(resetButton);
 }
+
 function placeMarker(e) {
   const row = Number(e.target.dataset.row);
   const column = Number(e.target.dataset.column);
-  let gameStatus = gameController.playRound(row, column);
+  var gameStatus = gameController.playRound(row, column);
   e.target.textContent = gameController.gameBoard.getBoard()[row][column];
   if (gameStatus.gameOver) {
     displayGameStatus(gameStatus);
   }
   else if (gameController.getActivePlayer().isBot) {
-
     botClick();
   }
 }
 
 function displayGameStatus(gameStatus) {
-  let gameStatusElement = document.querySelector('.gameStatus');
+  var gameStatusElement = document.querySelector('.gameStatus');
   removeListenersFromButtons();
   if (gameStatus.winner !== null) {
     gameStatusElement.textContent = `${gameStatus.winner.getName()} has won the Game`;
@@ -241,24 +248,35 @@ function displayGameStatus(gameStatus) {
     gameStatusElement.textContent = "The game is a tie";
   }
 }
+
 function resetGame() {
   gameController.resetGame();
-  let gameStatusElement = document.querySelector('.gameStatus');
+  var gameStatusElement = document.querySelector('.gameStatus');
   gameStatusElement.textContent = '';
+  container.removeChild(document.querySelector('.reset'))
   displayBoard(gameController.gameBoard.getBoard());
   if (gameController.getActivePlayer().isBot) {
     botClick();
   }
 }
+
 function removeListenersFromButtons() {
-  let buttons = document.querySelectorAll('button');
+  var buttons = document.querySelectorAll('button');
   buttons.forEach(button => {
     button.removeEventListener('click', placeMarker);
   })
 }
+
 function botClick() {
   var cell = gameController.getActivePlayer().chooseCell();
   var button = document.querySelector(`button[data-row='${cell.row}'][data-column='${cell.column}']`);
   button.click();
 }
+
+function getUserName() {
+  const name = nameInput.value;
+  container.inert = false;
+  gameController.getActivePlayer().setName(name);
+}
+
 displayBoard(gameController.gameBoard.getBoard())
